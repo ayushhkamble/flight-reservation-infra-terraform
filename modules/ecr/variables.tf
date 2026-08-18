ca721de526f99a23fd2d808992ff0298acd135b6
ayush <ayushkamble820@gmail.com>
2026-08-18 15:18:12 +0530

    ecr modules added

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,10 @@
+variable "repository_name" {
+  description = "Name of the ECR repository"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
